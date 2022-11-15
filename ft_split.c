f7d37b132f43f1340cc3235964e9ed9b70580eef
Clemence Llovio <cllovio@z4r8p1.42lyon.fr>
2022-11-15 13:34:48 +0100

    substr, split, .h, Makefile

---
+++ ft_split.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cllovio <cllovio@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 11:26:17 by cllovio           #+#    #+#             */
+/*   Updated: 2022/11/15 13:12:24 by cllovio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int find_c(char const *s, char c, int i)
+{
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**split;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (s[i])
+	{
+		while (find_c(s, c, i) == 0)
+		{
+			split[j][k] = s[i];
+			i++;
+			k++;
+		}
+		k = 0;
+		j++;
+		i++;
+	}
+}
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cllovio <cllovio@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 13:49:31 by cllovio           #+#    #+#             */
-/*   Updated: 2022/11/14 15:49:25 by cllovio          ###   ########.fr       */
+/*   Updated: 2022/11/15 11:20:44 by cllovio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	k = 0;
 	if (s == NULL)
 		return (NULL);
-	new_s = malloc(sizeof(char) * (len + 1));
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (ft_strlen(&s[start]) > len)
+		new_s = malloc(sizeof(char) * (len + 1));
+	else
+		new_s = malloc(sizeof(char) * (ft_strlen(&s[start]) + 1));
 	if (!(new_s))
 		return (NULL);
 	while (i < start)
